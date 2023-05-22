787e31aeb3f796b719b2f211831a5829b93e8f49
Bego96 <begic.ismar@gmail.com>
2023-05-22 02:03:16 +0200

    Commits

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3561b791dc164f8ca71d9008b556f5b0b9fab2af6f3afad49e1444c986df73b3
-size 2237
+oid sha256:d5925858fe65f5a65412dd5f9879c4ff1a8419525f4878f5fdffd88967260540
+size 2230
